a5160efeec34aabde5f1116183f9bb1392dd9a48
hiteshiyadav10 <hiteshiyadav10@gmail.com>
2026-03-21 14:51:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+ "use strict"; //treat all JS code as newer version
+
+ //alert( 3 + 3) //we are using nodejs not browser
+
+ console.log(3
+    +
+    3
+ ) //thus syntax has no problem but is a really bad practice.
+ //code readability should be high
+
+
+ let name = "hiteshi"
+ let age = 19
+ let isLoggedIn = false
+ let state;
+
+ // number datatype range => 2 to power 53
+ //bigint is for bigger numbers
+ //string is used in single or double quotes. Double quotes is more preferable.
+ //boolean - true/false
+ //null - standalone value
+ //undefined - value is not assigned yet
+ //symbol - unique
+
+
+ //object
+
+ console.log(typeof age);
+ console.log(typeof "hiteshi");
+ console.log(typeof state);
+ console.log(typeof null);  // it gives "object" as result(its also called a drawback of language )
+ console.log(typeof undefined); //gives "undefined"
+
+
