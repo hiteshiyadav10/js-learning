0797fb2b7c31aa277cf16926452bb50bf150e290
hiteshiyadav10 <hiteshiyadav10@gmail.com>
2026-08-06 11:39:12 +0000

    minor changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,11 +7,11 @@
 //console.log(3+3); console.log("Hiteshi")//now this will run but its still a bad practice to do
 
 //Code redabiity should be high
-console.log(3+3) 
-console.log("Hiteshi")
+console.log(3+3) //prints 6 in the terminal
+console.log("Hiteshi") //prints Hiteshi in the terminal
 
 //"Preetier" is an extension which is used to indent the code automatically
-//MDN easy documentation for everthing given by mozilla org. But original documentation is actually give by ECMAScript for giving js standards.
+//MDN is easy documentation for everthing given by mozilla org. But original documentation is actually given by ECMAScript for giving js standards.
 
 
 //Datatypes
@@ -20,15 +20,15 @@ let age = 20
 let isLoggedIn = true
 let state;
 let status = null
-//1.Number : its has range =>2 to power 53
+//Number : it has range =>2 to power 53
 //BigInt : used when your number is very big.
 //String : can use single or double quotes.
-//Boolean : uses true/false as its value.
+//Boolean : uses true/false only as its value.
 //Null : Yes its a dataType but its also a standalone value.Its a representation of empty value.
 //Undefined : when variable is declared but no value is given yet. Eg.-state
 //Symbol : its implementation will be seen during react topic. It is used to find uniqueness.
 
-//Object : willbe studied further.
+//Object : will be studied further.
 
 //Finding DataTypes
 console.log(typeof age) //will return number as answer
