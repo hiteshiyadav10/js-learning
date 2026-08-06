c6bbd58114aede9ca30e58de120e60451f3e1cd7
hiteshiyadav10 <hiteshiyadav10@gmail.com>
2026-08-06 11:14:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; //use all JS code as newer version
+
+
+//alert(3+3) //this will give an error because alert isn't defined in node.js environment,it is a browser function. we can run it in browser console or use node.js sepecific function like console.log() this will work in node.js environment.
+
+//console.log(3+3) console.log("Hiteshi")//this will give a error
+//console.log(3+3); console.log("Hiteshi")//now this will run but its still a bad practice to do
+
+//Code redabiity should be high
+console.log(3+3) 
+console.log("Hiteshi")
+
+//"Preetier" is an extension which is used to indent the code automatically
+//MDN easy documentation for everthing given by mozilla org. But original documentation is actually give by ECMAScript for giving js standards.
+
+
+//Datatypes
+let name = "Hiteshi"
+let age = 20
+let isLoggedIn = true
+let state;
+let status = null
+//1.Number : its has range =>2 to power 53
+//BigInt : used when your number is very big.
+//String : can use single or double quotes.
+//Boolean : uses true/false as its value.
+//Null : Yes its a dataType but its also a standalone value.Its a representation of empty value.
+//Undefined : when variable is declared but no value is given yet. Eg.-state
+//Symbol : its implementation will be seen during react topic. It is used to find uniqueness.
+
+//Object : willbe studied further.
+
+//Finding DataTypes
+console.log(typeof age) //will return number as answer
+console.log(typeof name)//wiil return string as answer
+console.log(typeof isLoggedIn)//will return boolean  as answer
+console.log(typeof state)//will return undefined only as answer
+console.log(typeof status)//will return object as answer
