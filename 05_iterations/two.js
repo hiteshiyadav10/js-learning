1c59da0d49239c4fbd9ccf3609890573afef7986
hiteshiyadav10 <hiteshiyadav10@gmail.com>
2026-09-09 14:08:15 +0000

    while and do-while  loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+//while
+let index=0;
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index+=2;
+}//0,2,4,6,8 and 10 gets printed
+
+let myArray = ["flash","batman","superman"]
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+//value is flash
+//value is batman
+//value is superman
+
+
+//do-while loop'
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score<=10);
+//score from 1 to 10 gets printed
+
+let points = 11
+do {
+    console.log(`points are ${points}`)
+    score++;
+} while (points<=10);//points are 11
